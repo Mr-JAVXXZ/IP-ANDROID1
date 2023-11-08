432c601c35d3685baf2a629cd215001034412eec
Mr-JAVXXZ <150129084+Mr-JAVXXZ@users.noreply.github.com>
2023-11-08 17:01:48 +0700

    Create IP-ANDROID1.py

---
+++ IP-ANDROID1.py
@@ -0,0 +1,30 @@
+import os
+import socket
+import time
+import device
+import kernel
+import uptime
+import cpu
+import gpu
+import ram
+
+os.system("clear")
+os.system("figlet IP CRACKER")
+
+os.system("screenfetch -A Korora")
+print "==============IP CRACKER==============="
+print "TYPE ANDROID =  % (type)"
+print "DEVICE = % (device)"
+print "ROM = % (rom)"
+print "KERNEL = % (kernel)"
+print "UPTIME = % (uptime)"
+print "CPU = % (cpu)"
+print "GPU = % (gpu)"
+print "RAM = % (ram)"
+print "======================================="
+Exit =raw_input("KETIK KELUAR UNTUK CLOSED DARI SCRITPT = ")
+print "LOADING ....."
+time.sleep(2)
+print "DONE"
+time.sleep(1)
+os.system("clear")
